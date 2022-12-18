ae44949ea7b5ba78f56252578255412a91fb343f
Ricardo Teixeira <47228282+kika2001@users.noreply.github.com>
2022-12-18 21:48:48 +0000

    All Art assets

---
+++ Ayahuasca/Assets/_Art/Polaris - Low Poly Ecosystem/Poseidon - Low Poly Water System/Runtime/Scripts/Effects/BuiltinRP/PWetLensRenderer.cs
@@ -0,0 +1,35 @@
+#if UNITY_POST_PROCESSING_STACK_V2
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace Pinwheel.Poseidon.FX.PostProcessing
+{
+    public class PWetLensRenderer : PostProcessEffectRenderer<PWetLens>
+    {
+        private static Shader wetLensShader;
+        private static Shader WetLensShader
+        {
+            get
+            {
+                if (wetLensShader==null)
+                {
+                    wetLensShader = Shader.Find(PWaterFX.WETLENS_SHADER_BRP);
+                }
+                return wetLensShader;
+            }
+        }
+
+        public override void Render(PostProcessRenderContext context)
+        {
+            PropertySheet sheet = context.propertySheets.Get(WetLensShader);
+            Texture normalMap = settings.normalMap.value ?? PPoseidonSettings.Instance.DefaultNormalMap;
+            sheet.properties.SetTexture(PMat.PP_WET_LENS_TEX, normalMap);
+            sheet.properties.SetFloat(PMat.PP_WET_LENS_STRENGTH, settings.strength * settings.intensity);
+
+            context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+        }
+    }
+}
+#endif
