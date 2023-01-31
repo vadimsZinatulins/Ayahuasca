fe2bef5711039bc1fa3f6fbd7bbbe4c12515ffef
Vadims Zinatlins <43823259+vadimsZinatulins@users.noreply.github.com>
2023-01-31 00:21:47 +0000

    Added animations for when the characters an inside the canoe (needs polishing) (#51)

--- Ayahuasca/Assets/_Scripts/Behaviours/Interfaces/IRidable.cs
+++ Ayahuasca/Assets/_Scripts/Behaviours/Interfaces/IRidable.cs
@@ -4,7 +4,7 @@ namespace _Scripts.Behaviours.Interfaces
 {
     public interface IRiddable
     {
-        public void StartRiding(GameObject go, ref PlayerRiding ridingType);
+        public void StartRiding(GameObject go, ref PlayerRiding ridingType, out RowingSide side);
         public bool StopRiding(GameObject go);
 
         public string GetRideText();
