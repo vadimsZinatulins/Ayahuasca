bcabd6499bea1fae69c5cea3fc2f048eb153a558
Ricardo Teixeira <47228282+kika2001@users.noreply.github.com>
2023-02-13 00:14:26 +0000

    Major fixes (#93)
    * FIxed metas
    
    * Fixed UI not changing numbers

--- Ayahuasca/Assets/_Scripts/InventoryUI.cs
+++ Ayahuasca/Assets/_Scripts/InventoryUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,22 +11,43 @@ public class InventoryUI : MonoBehaviour {
     [SerializeField] private GameObject centerInventory;
     [SerializeField] private GameObject bottomInventory;
 
+    [SerializeField] private List<InventoryText> InventoryTexts;
+
     void Awake() {
         Instance = this;
 
         SetSplitScreen(false);
     }
 
-    public void SetSplitScreen(bool isActive) {
+    public void SetSplitScreen(bool isActive)
+    {
         centerInventory.SetActive(isActive);
         bottomInventory.SetActive(!isActive);
     }   
 
-    public void SetValue(string herb, int amount) {
+    public void SetValue(string herb, int amount)
+    {
         SetHerbValue(centerInventory, herb, amount);
         SetHerbValue(bottomInventory, herb, amount);
     }
-    private void SetHerbValue(GameObject go, string herb, int amount) {
-        go.transform.Find(herb + " Text")?.GetComponent<TextMeshProUGUI>()?.SetText(herb + ": " + amount);
+    private void SetHerbValue(GameObject go, string herb, int amount)
+    {
+        foreach (var inventoryText in InventoryTexts)
+        {
+            if (inventoryText.title == herb)
+            {
+                inventoryText.centerTextMesh.SetText(amount.ToString());
+                inventoryText.bottomTextMesh.SetText(amount.ToString());
+                break;
+            }
+        }
     } 
+}
+
+[Serializable]
+public class InventoryText
+{
+    public string title;
+    public TextMeshProUGUI centerTextMesh;
+    public TextMeshProUGUI bottomTextMesh;
 }
